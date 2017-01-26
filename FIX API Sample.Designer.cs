d9a7757a785298a22a4fd7858355179e58c25649
Spotware <connect@spotware.com>
2017-01-26 17:57:59 +0200

    Add project files.

---
+++ FIX API Sample.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FIX_API_Sample
+{
+    partial class frmFIXAPISample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogon = new System.Windows.Forms.Button();
+            this.lblLogonResponce = new System.Windows.Forms.Label();
+            this.btnHeartbeat = new System.Windows.Forms.Button();
+            this.lblHeartbeatMessage = new System.Windows.Forms.Label();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnMarketDataRequest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLogon
+            // 
+            this.btnLogon.Location = new System.Drawing.Point(12, 4);
+            this.btnLogon.Name = "btnLogon";
+            this.btnLogon.Size = new System.Drawing.Size(133, 23);
+            this.btnLogon.TabIndex = 0;
+            this.btnLogon.Text = "Logon";
+            this.btnLogon.UseVisualStyleBackColor = true;
+            this.btnLogon.Click += new System.EventHandler(this.btnLogon_Click);
+            // 
+            // lblLogonResponce
+            // 
+            this.lblLogonResponce.AutoSize = true;
+            this.lblLogonResponce.Location = new System.Drawing.Point(159, 33);
+            this.lblLogonResponce.Name = "lblLogonResponce";
+            this.lblLogonResponce.Size = new System.Drawing.Size(0, 13);
+            this.lblLogonResponce.TabIndex = 1;
+            // 
+            // btnHeartbeat
+            // 
+            this.btnHeartbeat.Location = new System.Drawing.Point(12, 33);
+            this.btnHeartbeat.Name = "btnHeartbeat";
+            this.btnHeartbeat.Size = new System.Drawing.Size(133, 23);
+            this.btnHeartbeat.TabIndex = 2;
+            this.btnHeartbeat.Text = "Send Test Request";
+            this.btnHeartbeat.UseVisualStyleBackColor = true;
+            this.btnHeartbeat.Click += new System.EventHandler(this.btnHeartbeat_Click);
+            // 
+            // lblHeartbeatMessage
+            // 
+            this.lblHeartbeatMessage.AutoSize = true;
+            this.lblHeartbeatMessage.Location = new System.Drawing.Point(12, 33);
+            this.lblHeartbeatMessage.Name = "lblHeartbeatMessage";
+            this.lblHeartbeatMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblHeartbeatMessage.TabIndex = 3;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(12, 91);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(133, 23);
+            this.btnLogout.TabIndex = 4;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // btnMarketDataRequest
+            // 
+            this.btnMarketDataRequest.Location = new System.Drawing.Point(12, 62);
+            this.btnMarketDataRequest.Name = "btnMarketDataRequest";
+            this.btnMarketDataRequest.Size = new System.Drawing.Size(133, 23);
+            this.btnMarketDataRequest.TabIndex = 5;
+            this.btnMarketDataRequest.Text = "Market Data Request";
+            this.btnMarketDataRequest.UseVisualStyleBackColor = true;
+            this.btnMarketDataRequest.Click += new System.EventHandler(this.btnMarketDataRequest_Click);
+            // 
+            // frmFIXAPISample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 261);
+            this.Controls.Add(this.btnMarketDataRequest);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.lblHeartbeatMessage);
+            this.Controls.Add(this.btnHeartbeat);
+            this.Controls.Add(this.lblLogonResponce);
+            this.Controls.Add(this.btnLogon);
+            this.Name = "frmFIXAPISample";
+            this.ShowIcon = false;
+            this.Text = "Fix API Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogon;
+        private System.Windows.Forms.Label lblLogonResponce;
+        private System.Windows.Forms.Button btnHeartbeat;
+        private System.Windows.Forms.Label lblHeartbeatMessage;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnMarketDataRequest;
+    }
+}
+
---
+++ FIX API Sample.cs
@@ -0,0 +1,91 @@
+﻿using FIX_API_Library;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIX_API_Sample
+{
+    public partial class frmFIXAPISample : Form
+    {
+    
+        private int _port = 5201;
+
+        //roboforex
+        private string _host = "217.79.189.190";
+        private string _username = "4064539";
+        private string _password = "m3e1d500D";
+        private string _senderCompID = "roboforex.4064539";
+        private string _senderSubID = "4064539";
+        // Test Server
+        //private string _host = "proxy-qa.dev.cs.spotwa.re";
+        //private string _username = "1012";
+        //private string _password = "4";
+        //private string _senderCompID = "local.1012";
+        //private string _senderSubID = "1012";
+
+
+        private string _targetCompID = "CSERVER";
+        private int _messageSequenceNumber = 1;
+        private int _testRequestID = 1;
+        TcpClient _client;
+        NetworkStream _stream;
+        public frmFIXAPISample()
+        {
+            InitializeComponent();
+            _client = new TcpClient(_host, _port);
+            _stream = _client.GetStream();
+        }
+
+        private void btnLogon_Click(object sender, EventArgs e)
+        {
+            var messageConstructor = new MessageConstructor(_host, _username, _password, _senderCompID, _senderSubID, _targetCompID);
+            var message = messageConstructor.LogonMessage(MessageConstructor.SessionQualifier.QUOTE, _messageSequenceNumber,0,true);            
+            lblLogonResponce.Text = SendMessage(message);
+        }
+
+        private string SendMessage(string message)
+        {
+            var byteArray = Encoding.ASCII.GetBytes(message);
+            var client = new TcpClient(_host, _port);
+            var stream = client.GetStream();           
+            stream.Write(byteArray, 0, byteArray.Length);
+            Thread.Sleep(100); 
+            var buffer = new byte[1024];
+            if(stream.DataAvailable)
+                stream.Read(buffer, 0, 1024);
+            _messageSequenceNumber++;
+            var returnMessage = Encoding.ASCII.GetString(buffer);
+            return returnMessage;
+        }
+
+        private void btnHeartbeat_Click(object sender, EventArgs e)
+        {
+            var messageConstructor = new MessageConstructor(_host, _username, _password, _senderCompID, _senderSubID, _targetCompID);
+            var message = messageConstructor.TestRequestMessage(MessageConstructor.SessionQualifier.QUOTE, _messageSequenceNumber,_testRequestID);
+            _testRequestID++;
+           lblHeartbeatMessage.Text = SendMessage(message);
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            var messageConstructor = new MessageConstructor(_host, _username, _password, _senderCompID, _senderSubID, _targetCompID);
+            var message = messageConstructor.LogoutMessage(MessageConstructor.SessionQualifier.QUOTE, _messageSequenceNumber);           
+            lblLogonResponce.Text = SendMessage(message);
+        }
+
+        private void btnMarketDataRequest_Click(object sender, EventArgs e)
+        {
+            var messageConstructor = new MessageConstructor(_host, _username, _password, _senderCompID, _senderSubID, _targetCompID);
+            var message = messageConstructor.MarketDataRequestMessage(MessageConstructor.SessionQualifier.QUOTE, _messageSequenceNumber, "EURUSD:WDqsoT", 1,0,0,1,1);
+            lblLogonResponce.Text = SendMessage(message);
+        }
+    }
+}
